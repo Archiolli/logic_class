b6339b037a9b9d779fd27c21d556627964833bef
Archiolli <108499599+Archiolli@users.noreply.github.com>
2022-07-01 16:00:07 -0300

    Add files via upload

---
+++ Aluno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ExercicioClasses3
+{
+    class Aluno
+    {
+        public float Nota1;
+        public float Nota2;
+        public float Nota3;
+        public string Nome;
+        
+
+        public float NotaFinal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public override string ToString()
+        {
+            return NotaFinal().ToString();
+        }
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace ExercicioClasses3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Aluno x = new Aluno();
+
+            Console.WriteLine("================================================");
+            Console.WriteLine("             Digite o nome do aluno");
+            Console.WriteLine("================================================");
+            x.Nome = Console.ReadLine();
+            Console.Clear();
+            Console.Write("Digite  as três notas do aluno (1):  ");
+            x.Nota1 = float.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Digite  as três notas do aluno (2):  ");
+            x.Nota2 = float.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Digite  as três notas do aluno (3):  ");
+            x.Nota3 = float.Parse(Console.ReadLine());
+            Console.Clear();
+
+            
+
+
+
+        }
+    }
+}
